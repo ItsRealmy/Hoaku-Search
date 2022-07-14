200459fc9af3e599dacf7326d9a6d45fb62a5091
Realmy <43610767+ItsRealmy@users.noreply.github.com>
2022-07-14 10:14:43 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const ejs = require('ejs');
+const search = require('googlekit');
+const path = require('node:path');
+
+const app = express();
+const { PORT } = process.env;
+
+app.listen(PORT || 3400, () => {
+  console.log(`Listening on :${PORT || 3400}`)
+});
+
+app.get('/', async (req, res) => {
+  const completedTemplate = await ejs.renderFile(path.join(__dirname, 'views/index.ejs'));
+
+  return res.send(completedTemplate);
+});
+
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use('/static', express.static(path.join(__dirname, 'static')));

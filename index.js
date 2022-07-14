1aaa70cb69798b732678963d156a22f64569c2d8
Realmy <43610767+ItsRealmy@users.noreply.github.com>
2022-07-14 10:44:32 +0200

    Basic search engine

--- index.js
+++ index.js
@@ -17,5 +17,27 @@ app.get('/', async (req, res) => {
   return res.send(completedTemplate);
 });
 
+app.get('/search', async (req, res) => {
+  const { q: query } = req.query;
+
+  if (!query) {
+    const completedTemplate = await ejs.renderFile(path.join(__dirname, 'views/index.ejs'));
+
+    return res.send(completedTemplate);
+  } else {
+    let page = parseInt(req.query.p) || 0;
+    if (isNaN(page)) return res.send('Error');
+    if (page < 0) return res.send('Error');
+
+    const resp = await search(query, page);
+
+    const completedTemplate = await ejs.renderFile(path.join(__dirname, 'views/search.ejs'), {
+      results: resp.results,
+    });
+
+    return res.send(completedTemplate);
+  }
+});
+
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.use('/static', express.static(path.join(__dirname, 'static')));
